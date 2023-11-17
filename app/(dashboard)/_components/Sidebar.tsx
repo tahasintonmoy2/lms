e95e888aac20f2c23c02c49ec48bfb35d811fe3e
Tahasin <110173793+tahasintonmoy2@users.noreply.github.com>
2023-11-17 11:24:31 +0600

    update

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
       <div className="h-full border-r flex flex-col bg-white overflow-y-auto shadow-sm">
         <div className="p-6 flex items-center">
           <Icon />
-          <p className="px-2 text-blue-600 text-xl font-semibold">Lambdasted</p>
+          <p className="px-2 text-blue-600 text-xl font-semibold">Next Stack</p>
         </div>
         <div className="flex flex-col w-full">
             <SidebarRoutes/>
